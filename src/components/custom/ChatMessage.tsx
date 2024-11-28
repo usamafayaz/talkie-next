67422165aca96051d337451db196e893596a8fbb
usamafayaz <usama.fayyaz157@gmail.com>
2024-11-28 22:01:12 +0500

    Fixed the image preview for mobile view

--- src/components/custom/ChatMessage.tsx
+++ src/components/custom/ChatMessage.tsx
@@ -24,17 +24,17 @@ const ImageModal: React.FC<{
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/80"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 p-4"
       onClick={onClose}
     >
-      <div className="relative max-w-[90vw] max-h-[90vh]">
+      <div className="flex items-center justify-center p-4 relative max-w-[90vw] max-h-[90vh]">
         <button
           onClick={onClose}
           className="absolute -top-10 right-0 p-2 text-white hover:text-gray-300"
         >
           <X size={24} />
         </button>
-        <div className="relative w-full h-full min-h-[300px] min-w-[700px]">
+        <div className="relative w-full h-full md:min-h-[300px] md:min-w-[700px] min-h-[300px] min-w-[700px] rounded-lg overflow-hidden">
           <Image
             src={imageSrc}
             alt="Enlarged message image"
